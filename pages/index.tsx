7c262268208b3dd1770d2021000a726f3858a68e
ipriyam26 <ipriyam26@gmail.com>
2022-08-01 19:13:52 +0530

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import type { NextPage } from "next";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Block from "../components/block";
+import Product from "../types/Product";
+
+const Home: NextPage<{products:Product[]}> = ({products}) => {
 
 
-const Home: NextPage = () => {
-  return(
-    <div></div>
-  );
+  return <div>
+
+    <Block products={products}/>
+  </div>;
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+
+  let response = await fetch('https://fakestoreapi.com/products?limit=6')
+  let data:Product[] = await response.json()
+  console.log(data.length);
+  
+  return {
+    props: {
+      products: data
+    }
+  };
+};
---
+++ types/Product.ts
@@ -0,0 +1,11 @@
+
+
+export default interface Product{
+    id:number,
+    title:string,
+    price:number,
+    description:string,
+    category:string,
+    image:string,
+    rating:{rate:number,count:number}
+}
